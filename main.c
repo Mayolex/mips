55a3c9247b6899540055361b570b9a187daf8db4
187Lucas <lucasbozon@gmail.com>
2020-10-27 22:56:34 +0000

    remplacement fscanf par fgets, mise en commentaire des printf

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 tests/in2.txt->traduction->out_hex.txt
 */
 int main(int argc, char *argv[]){
-
+    char instr[100];/*="ADDI $1,$0,3 #commentaire";*/
     if(argc==3){
         FILE *readFile=fopen(argv[1],"r");
         if(readFile == NULL){/*test ouverture fichier*/
@@ -22,14 +22,11 @@ int main(int argc, char *argv[]){
             perror("erreur a l'ouverture du fichier d'écriture\n");
             exit(1);
         }
-        char instr[100];
-        /*
-        en lisant avec le fscanf, pas besoin de cut_instruction()
-        */
-        fscanf(readFile,"%s", instr);
-        printf("instr : %s\n",instr);
+        
+        fgets(instr,100,readFile);
+        /*printf("instr : %s\n",instr);*/
         instruction *a = cut_instruction(instr);
-        printf("%s\n%s\n%s\n%s\nto int: %d\n",a->opcode,a->op1,a->op2,a->op3,registerToInt(a->op1));
+        /*printf("%s\n%s\n%s\n%s\nto int: %d\n",a->opcode,a->op1,a->op2,a->op3,registerToInt(a->op1));*/
         translate_instruction(a,"instructiontohex.txt",readFile,writeFile);
         fclose(readFile);
         fclose(writeFile);
