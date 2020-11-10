95f42754da12dcbf1b21fd6dbc907dcb31740b3c
187Lucas <lucasbozon@gmail.com>
2020-11-10 11:59:25 +0000

    début de operation()

--- instructions.c
+++ instructions.c
@@ -25,3 +25,85 @@ numinstruction *readinstr(unsigned int addr,memory_struct *mem){
     return(instr);
 }
 
+void ADDI(numinstruction *addi,register_struct *reg){
+    reg->registers[addi->rt]=(reg->registers[addi->rs])+(addi->immediate);
+}
+void AND(numinstruction *and,register_struct *reg){
+    reg->registers[and->rd]=(reg->registers[and->rs])&(reg->registers[and->rt]);
+}
+void BEQ(numinstruction *beq,register_struct *reg){
+    if((reg->registers[beq->rs])==(reg->registers[beq->rt])){
+        pc+=(beq->offset);
+    }
+}
+void BGTZ(numinstruction *bgtz,register_struct *reg){
+    if((reg->registers[bgtz->rs])>0){
+        pc+=(bgtz->offset);
+    }
+}
+void BLEZ(numinstruction *blez,register_struct *reg){
+    if((reg->registers[blez->rs])<=0){
+        pc+=(blez->offset);
+    }
+}
+void BNE(numinstruction *bne,register_struct *reg){
+    if((reg->registers[bne->rs])!=(reg->registers[bne->rt])){
+        pc+=(bne->offset);
+    }
+}
+void DIV(numinstruction *div,register_struct *reg){
+    reg->registers[LO]=(reg->registers[div->rs])/(reg->registers[div->rt]);
+    reg->registers[HI]=reg->registers[div->rs])%(reg->registers[div->rt]);
+}
+void J(numinstruction *j,register_struct *reg)
+void JAL(numinstruction *jal,register_struct *reg)
+void JR(numinstruction *jr,register_struct *reg)
+void LUI(numinstruction *lui,register_struct *reg)
+void LW(numinstruction *lw,register_struct *reg)
+void MFHI(numinstruction *mfhi,register_struct *reg)
+void MFLO(numinstruction *mflo,register_struct *reg)
+void MULT(numinstruction *mult,register_struct *reg)
+void NOP(numinstruction *nop,register_struct *reg)
+void OR(numinstruction *or,register_struct *reg)
+void ROTR(numinstruction *rotr,register_struct *reg)
+void SLL(numinstruction *sll,register_struct *reg)
+void SLT(numinstruction *slt,register_struct *reg)
+void SRL(numinstruction *srl,register_struct *reg)
+void SUB(numinstruction *sub,register_struct *reg)
+void SW(numinstruction *sw,register_struct *reg)
+void SYSCALL(numinstruction *syscall,register_struct *reg)
+void XOR(numinstruction *xor,register_struct *reg)
+
+void operation(numinstruction *instr){
+    switch(instr->op){
+        case 0x08://ADDI
+            ADDI(instr);
+            break;
+        case 0x24://AND
+            AND(instr);
+            break;
+        case 0x04://BEQ
+        case 0x07://BGTZ
+        case 0x06://BLEZ
+        case 0x05://BNE
+        case 0x1A://DIV
+        case 0x02://J
+        case 0x03://JAL
+        case 0x08://JR
+        case 0x0F://LUI
+        case 0x23://LW
+        case 0x10://MFHI
+        case 0x12://MFLO
+        case 0x18://MULT
+        case 0x00://NOP
+        case 0x25://OR
+        case 0x02://ROTR
+        case 0x00://SLL
+        case 0x2A://SLT
+        case 0x02://SRL
+        case 0x22://SUB
+        case 0x2B://SW
+        case 0x0C://SYSCALL
+        case 0x26://XOR
+    }
+}
--- instructions.h
+++ instructions.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "memory.h"
+#define LO 32
+#define HI 33
 typedef struct numinstruction numinstruction;
 struct numinstruction{
     int op; //voir ligne dans instructiontohex.txt
@@ -10,7 +12,35 @@ struct numinstruction{
     int sa;
     int immediate;
     int target;
+    int offset;
 };
 
-/* renvoi une instruction depuis une instruction stocké en mémoire a l'adresse donnée */
+/* renvoit une instruction depuis une instruction stockée en mémoire à l'adresse donnée */
 numinstruction *readinstr(unsigned int addr,memory_struct *mem);
+
+
+void ADDI(numinstruction *addi,register_struct *reg);
+void AND(numinstruction *and,register_struct *reg);
+void BEQ(numinstruction *beq,register_struct *reg);
+void BGTZ(numinstruction *bgtz,register_struct *reg);
+void BLEZ(numinstruction *blez,register_struct *reg);
+void BNE(numinstruction *bne,register_struct *reg);
+void DIV(numinstruction *div,register_struct *reg);
+void J(numinstruction *j,register_struct *reg);
+void JAL(numinstruction *jal,register_struct *reg);
+void JR(numinstruction *jr,register_struct *reg);
+void LUI(numinstruction *lui,register_struct *reg);
+void LW(numinstruction *lw,register_struct *reg);
+void MFHI(numinstruction *mfhi,register_struct *reg);
+void MFLO(numinstruction *mflo,register_struct *reg);
+void MULT(numinstruction *mult,register_struct *reg);
+void NOP(numinstruction *nop,register_struct *reg);
+void OR(numinstruction *or,register_struct *reg);
+void ROTR(numinstruction *rotr,register_struct *reg);
+void SLL(numinstruction *sll,register_struct *reg);
+void SLT(numinstruction *slt,register_struct *reg);
+void SRL(numinstruction *srl,register_struct *reg);
+void SUB(numinstruction *sub,register_struct *reg);
+void SW(numinstruction *sw,register_struct *reg);
+void SYSCALL(numinstruction *syscall,register_struct *reg);
+void XOR(numinstruction *xor,register_struct *reg);
