b44b3df5fae703150782ea521f134bda962a6220
Mayolex <yohann.brugerie@gmail.com>
2020-11-10 12:04:29 +0100

    print mémoire et registre

--- memory.c
+++ memory.c
@@ -32,14 +32,51 @@ int rw(memory_struct *mem_struct,unsigned int addr){
     return(rtrn);
 }
 
+void printRegisters(register_struct *reg_struct){
+    int i = 0;
+    printf("REGISTER STATES:\n\n");
+    printf("----------------------------------------------------------\n");
 
-/*int main(){
-    memory_struct mem;
+    while(i<NBREG){
+        printf("$%02d: %-8x  ",i,reg_struct->registers[i]);
+        i++;
+        while(i%4!=0 && i<NBREG){
+        printf("$%02d: %-8x  ",i,reg_struct->registers[i]);
+        i++;
+        }
+        printf("\n");
+    }
+    printf("----------------------------------------------------------\n");
+    printf("\n\n");
+
+}
+
+void printMemory(memory_struct *mem){
+        int i = 0;
+    printf("Memory STATES:\n\n");
+    printf("----------------------------------------------------------\n");
+
+    while(i<LENGH){
+        printf("@%08X: %-8X  ",i,rw(mem,i));
+        i+=4;
+        while(i%16!=0 && i<LENGH){
+        printf("@%08X: %-8X  ",i,rw(mem,i));
+        i+=4;
+        }
+        printf("\n");
+    }
+    printf("----------------------------------------------------------\n");
+    printf("\n\n");
+
+}
+
+
+int main(){
     register_struct regs;
+    memory_struct mem;
     regs.registers[0]=32000;
-    regs.registers[1]=15;//contient l'adresse
-    sw(&mem,&regs,1,0,1);
-    lw(&mem,&regs,1,2,1);
-    printf("%d",regs.registers[2]);
+    regs.registers[1]=15;//
+    printRegisters(&regs);
+    printMemory(&mem);
     return (0);
-}*/
+}
--- memory.h
+++ memory.h
@@ -1,4 +1,5 @@
 #define LENGH 1024
+#define NBREG 35
 /* 32 registres generaux + pc + HI + LO */
 
 typedef struct memory_struct memory_struct;
@@ -8,10 +9,12 @@ struct memory_struct{
 };
 
 struct register_struct{
-    unsigned int registers[35];
+    unsigned int registers[NBREG];
 };
 
 /*indbase: indice du registre adresse dans la mémoire  indrt : indice du registre*/
 void lw(memory_struct *mem_struct,register_struct *reg_struct ,int indbase,int indrt, short int offset);
 void sw(memory_struct *mem_struct,register_struct *reg_struct ,int indbase,int indrt, short int offset);
 int rw(memory_struct *mem_struct,unsigned int addr);
+void printRegisters(register_struct *reg_struct);
+void printMemory(memory_struct *memory_struct);
